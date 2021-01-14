9bb652fc1845d4c5a26e2936b4662e226efa295c
YUNES <62850554+yushu2020@users.noreply.github.com>
2021-01-13 20:25:38 -0800

    afd

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('set DISABLE_COLLECTSTATIC=1', 'project_settings.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_settings.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
